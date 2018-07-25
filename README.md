652abff4c189f8e16e473edd541dfc707d30ffed
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2018-07-25 17:01:24 +0200

    plot leakage current correction

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 This tool is implemented for the communication with CMS OMDS database and customized plotter on the pixel leakage currents. 
 
 First step:
-One needs to log on lxplus in order to include the Oracle base dependence. Then get the data points of pixel currents from the CMS database:
+One needs to log on lxplus in order to include the Oracle base dependence. Then get the last data points of pixel currents from the CMS database within the specified time window:
 
 python getCurrentFromDB.py
 
 Second step:
 Compute the mean value and RMS of pixel leakage current for each layer during a specific period.
 
-python currentReader.py
+python leakageCurrentReader.py
 
 Third step:
 Use pyROOT to plot the leakage current dependence.
 
-python currentPlotter.py
+python leakageCurrentPlotter.py
 
 Enjoy!
--- currentReader.py
+++ currentReader.py
@@ -1,33 +1,4 @@
-from numberOfROCs import *
-from phimap import *
 import os
-import ROOT
-from ROOT import TH1, TH1D
-
-def getLeakageCurrent(alias, currentValue):
-    rocCurrent = 0.
-
-    for k in numberOfRocs.keys():
-
-        if k.find(alias)!=-1:
-            rocCurrent = currentValue/float(numberOfRocs[k])
-            #rocCurrent = currentValue/float(numberOfRocs[k])*16.0 # each module has 16 rocs
-            break
-
-    return rocCurrent
-
-
-def getPhi(alias):
-
-    phi = -999.0
-
-    for j in phimap.keys():
-        if j.find(alias)!=-1:
-            phi = float(phimap[j])
-            break
-
-    return phi
-
 
 cylinder = ["BpI","BpO","BmI","BmO"]
 sector   = ["S1","S2","S3","S4","S5","S6","S7","S8"]
@@ -36,10 +7,8 @@ layer    = ["LAY1","LAY2","LAY3","LAY4"]
 #rog      = ["ROG1","ROG2","ROG3","ROG4"]
 #ring     = ["RNG1","RNG2"]
 
-#Colors = [9, 8, 7, 6, 5, 4, 3, 2]
-
 fileName = "currentsFromDB.txt"
-outputFile = "leakageCurrents.txt"
+outputFile = "currents.txt"
 
 if(os.path.exists(fileName)):
     fin = open(fileName, "r+")
@@ -52,37 +21,63 @@ if(os.path.exists(fileName)):
             for index3 in layer:
 
                 alias = "PixelBarrel_" + index1 + "_" + index2 + "_" + index3
-                phi = getPhi(alias)
-                currentHist = ROOT.TH1D("leakageCurrent", "leakageCurrent", 200, 0, 100)
+                hvCurrent = -999.
+                anaCurrent = -999.
+                digCurrent = -999.
 
                 for l in lines:
                     line = l.split()
 
-                    if "LAY1/channel002" in line[0]:
-                        aliasin = line[0].replace("LAY1/channel002","LAY1")
+                    aliasHV = "null"
+                    aliasLV1 = "null"
+                    aliasLV2 = "null"
 
-                    elif "LAY1/channel003" in line[0]:
-                        aliasin = line[0].replace("LAY1/channel003","LAY4")
+                    if "LAY14/channel002" in line[0]:
+                        aliasHV = line[0].replace("LAY14/channel002","LAY1")
 
-                    elif "LAY3/channel002" in line[0]:
-                        aliasin = line[0].replace("LAY3/channel002","LAY3")
+                    if "LAY14/channel003" in line[0]:
+                      aliasHV = line[0].replace("LAY14/channel003","LAY4")
 
-                    elif "LAY3/channel003" in line[0]:
-                        aliasin = line[0].replace("LAY3/channel003","LAY2")
+                    if "LAY23/channel002" in line[0]:
+                      aliasHV = line[0].replace("LAY23/channel002","LAY3")
 
-                    if aliasin == alias:
-                        rocCurrent = getLeakageCurrent(alias, float(line[1]))
-                        currentHist.Fill(rocCurrent)
+                    if "LAY23/channel003" in line[0]:
+                        aliasHV = line[0].replace("LAY23/channel003","LAY2")
 
-                if currentHist.GetEntries() == 0:
-                    fout.write(alias + "   " + str(phi) + "   " + str(0) + "   " + str(0) + "\n")
+
+                    if "LAY14/channel000" in line[0]:
+                        aliasLV1 = line[0].replace("LAY14/channel000","LAY1Dig")
+                        aliasLV2 = line[0].replace("LAY14/channel000","LAY4Dig")
+
+                    if "LAY14/channel001" in line[0]:
+                        aliasLV1 = line[0].replace("LAY14/channel001","LAY1Ana")
+                        aliasLV2 = line[0].replace("LAY14/channel001","LAY4Ana")
+
+                    if "LAY23/channel000" in line[0]:
+                        aliasLV1 = line[0].replace("LAY23/channel000","LAY2Dig")
+                        aliasLV2 = line[0].replace("LAY23/channel000","LAY3Dig")
+
+                    if "LAY23/channel001" in line[0]:
+                        aliasLV1 = line[0].replace("LAY23/channel001","LAY2Ana")
+                        aliasLV2 = line[0].replace("LAY23/channel001","LAY3Ana")
+
+
+                    if aliasHV == alias:
+                        hvCurrent = float(line[1])
+
+                    if (alias in aliasLV1) or (alias in aliasLV2):
+                        if ("Dig" in aliasLV1) or ("Dig" in aliasLV2):
+                            digCurrent = float(line[1])
+                        if ("Ana" in aliasLV1) or ("Ana" in aliasLV2):
+                            anaCurrent = float(line[1])
+
+
+                if hvCurrent == -999. or digCurrent == -999. or anaCurrent == -999.:
+                    print alias, " has empty content!  HV: ", hvCurrent, "  Dig: ", digCurrent, "  Ana: ", anaCurrent
+                    fout.write(alias + "   " + str(hvCurrent) + "   " + str(digCurrent) + "   " + str(anaCurrent) + "\n")
 
                 else:
-                    averageCurrent = currentHist.GetMean()
-                    uncCurrent = currentHist.GetRMS()
-                    fout.write(alias + "   " + str(phi) + "   " + str(rocCurrent) + "   " + str(uncCurrent) + "\n")
-
-                currentHist.Reset()
+                    fout.write(alias + "   " + str(hvCurrent) + "   " + str(digCurrent) + "   " + str(anaCurrent) + "\n")
 
 
     fin.close()
